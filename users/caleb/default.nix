12ba7aeffd18fd5fd60e16eb2981d9a24e363388
Caleb Schmucker <9125543+cheronobyl@users.noreply.github.com>
2020-05-11 21:53:41 -0700

    Re-added shell declaration but commented out

--- users/caleb/default.nix
+++ users/caleb/default.nix
@@ -3,6 +3,7 @@
     isNormalUser = true;
     home = "/home/caleb";
     description = "Caleb Schmucker";
+#   shell = pkgs.fish; #Temporarily disabled due to failures
     extraGroups = [ "wheel" "libvirtd" ]; # Enable ‘sudo’ for the user.
     initialPassword = ""; # Should be given a real password ASAP
   };
